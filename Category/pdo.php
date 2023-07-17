e82f854e3a79c66c9ea1c1eb8303ef6bb24320f3
Vyen5602 <vuhaiyen562002@gmail.com>
2023-07-17 21:21:31 +0700

    Update pdo.php

--- Category/pdo.php
+++ Category/pdo.php
@@ -1,57 +1,58 @@
 <?php
-    require_once "pdo.php";
-    $categoryConnection= new CategoryConnection();
-    $category=$categoryConnection->getData();
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Category</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container mt-3">
-        <div>
-            <h3>List Categories</h3>
-            <a href="add.php" class="btn btn-success" style="margin-right: 5px;">Create</a>
-        </div>
-        <table class="table table-hover">
-        <thead>
-            <tr>
-            <th scope="col">STT</th>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                $stt = 1;
-                foreach($category as $value): ?>
-            <tr>
-                <td><?= $stt++; ?></td>
-                <td><?= $value['id'];?></td>
-                <td><?= $value['name'];?></td>
-                <td>
-                    <form id="delete_<?= $value['id'] ?>" action="delete.php" method="POST" style="display:flex">
-                        <a href="./edit.php?id=<?= $value['id']?>" class="btn btn-dark" style="margin-right: 5px">Edit</a>
-                        <input type="hidden" value="<?= $value['id'] ?>" name="id">
-                        <a class="btn btn-dark" onclick="confirmDelete(<?= $value['id'] ?>)">Delete</a>
-                    </form>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-        </table>  
-    </div>
-    <script>
-    function confirmDelete(id) {
-        let result = confirm('Are you sure?');
-        if (result === true) {
-            document.getElementById(`delete_${id}`).submit();
+abstract class Connection{
+    protected $connection,
+    $host,
+    $dbname,
+    $username,
+    $password;
+    public function __construct(){
+        $this->host = 'localhost';
+        $this->dbname = 'test';
+        $this->username = 'root';
+        $this->password = '';
+        $this->connection = $this->connect();
+    }
+    public function connect(){
+        try{
+            $connection = new PDO("mysql:host=$this->host;dbname=$this->dbname",$this->username, $this->password);
+            $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $connection;
+        }
+        catch (Exception $e) {
+            die($e->getMessage());     
         }
     }
-</script>
-</body>
-</html>
+    public function prepareSQL($sql){
+        return $this->connection->prepare($sql);
+    }
+}
+class Category extends Connection{
+    public function getData(){
+        $sql = "SELECT * FROM category";
+        $select = $this->prepareSQL($sql);
+        $select->execute();
+        return $select->fetchAll();
+    }
+    public function getOneData($data){
+        $sql = "SELECT * FROM category WHERE id = :id";
+        $select = $this->prepareSQL($sql);
+        $select->execute($data);
+        return $select->fetchAll();
+    }
+    public function createNewData($data){
+        $sql = "INSERT INTO category (name) VALUES (:name)";
+        $create = $this->prepareSQL($sql);
+        $create->execute($data);
+    }
+    public function updateData($data){
+        $sql = "UPDATE category SET name = :name WHERE id = :id";
+        $update = $this->prepareSQL($sql);
+        $update->execute($data);
+    }
+    public function deleteData($data){
+        $sql = "DELETE FROM category WHERE id = :id";
+        $update = $this->prepareSQL($sql);
+        $update->execute($data);
+    }
+}
+?>
