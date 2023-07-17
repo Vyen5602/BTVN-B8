567f012d14f5c0c42d8750268fd2dead3fff79ea
Vyen5602 <vuhaiyen562002@gmail.com>
2023-07-17 20:58:35 +0700

    update

--- Category/pdo.php
+++ Category/pdo.php
@@ -1,50 +1,57 @@
 <?php
-$DB_TYPE = 'mysql';
-$DB_HOST = 'localhost';
-$DB_NAME = 'crud_test';
-$USER = 'root';
-$PW = '';
+    require_once "pdo.php";
+    $categoryConnection= new CategoryConnection();
+    $category=$categoryConnection->getData();
+?>
 
-try {
-    $connection = new PDO("$DB_TYPE:host=$DB_HOST;dbname=$DB_NAME", $USER, $PW);
-    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (Exception $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-
-function prepareSQL($sql) {
-    global $connection;
-    return $connection->prepare($sql);
-}
-
-function all() {
-    $sql = "SELECT * FROM categories";
-    $stmt = prepareSQL($sql);
-    $stmt->execute();
-    return $stmt->fetchAll();
-}
-
-function create($data) {
-    $sql = "INSERT INTO categories (name) VALUES (:name)";
-    $stmt = prepareSQL($sql);
-    $stmt->execute($data);
-}
-
-function update($data) {
-    $sql = "UPDATE categories SET name=:name WHERE id=:id";
-    $stmt = prepareSQL($sql);
-    $stmt->execute($data);
-}
-
-function delete($data) {
-    $sql = "DELETE FROM categories where id=:id";
-    $stmt = prepareSQL($sql);
-    $stmt->execute($data);
-}
-
-function findById($data) {
-    $sql = "SELECT * FROM categories where id=:id";
-    $stmt = prepareSQL($sql);
-    $stmt->execute($data);
-    return $stmt->fetch();
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Category</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-3">
+        <div>
+            <h3>List Categories</h3>
+            <a href="add.php" class="btn btn-success" style="margin-right: 5px;">Create</a>
+        </div>
+        <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">STT</th>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                $stt = 1;
+                foreach($category as $value): ?>
+            <tr>
+                <td><?= $stt++; ?></td>
+                <td><?= $value['id'];?></td>
+                <td><?= $value['name'];?></td>
+                <td>
+                    <form id="delete_<?= $value['id'] ?>" action="delete.php" method="POST" style="display:flex">
+                        <a href="./edit.php?id=<?= $value['id']?>" class="btn btn-dark" style="margin-right: 5px">Edit</a>
+                        <input type="hidden" value="<?= $value['id'] ?>" name="id">
+                        <a class="btn btn-dark" onclick="confirmDelete(<?= $value['id'] ?>)">Delete</a>
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+        </table>  
+    </div>
+    <script>
+    function confirmDelete(id) {
+        let result = confirm('Are you sure?');
+        if (result === true) {
+            document.getElementById(`delete_${id}`).submit();
+        }
+    }
+</script>
+</body>
+</html>
